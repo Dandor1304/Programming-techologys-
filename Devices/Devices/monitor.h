980e5c898e3854900ba2c1517b6df3cf34b9ff46
Dandor <dandoroshenko@mail.ru>
2021-12-02 00:16:29 +0300

    create h and cpp files

--- Devices/Devices/monitor.h
+++ Devices/Devices/monitor.h
@@ -1,7 +1,17 @@
 #pragma once
 #include "peripheral_devs.h"
+#include <iostream>
+#include <string.h>
+using namespace std;
+
 class monitor : public peripheral_devs
 {
-
+public:
+    monitor(string,int, string);
+    void setdiagonal(string);
+    void show();
+protected:
+private:
+    string diagonal;
 };
 
--- Devices/Devices/peripheral_devs.h
+++ Devices/Devices/peripheral_devs.h
@@ -1,5 +1,18 @@
 #pragma once
+#include <iostream>
+#include <string.h>
+using namespace std;
+
 class peripheral_devs
 {
-};
+public:
+        peripheral_devs();
+        int getcost();
+        virtual void show();
+        void setmaker(string);
+        void setcost(int);
+protected:
+        string maker;
+        int cost;
+ };
 
